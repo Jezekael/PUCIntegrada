39ac8f4a884dd135e05f6be1b2ec00a21e23682c
Jez <jezekael.brunon@gmail.com>
2024-10-09 11:25:18 +0000

    Cleaning

---
+++ src/AlunoMarketo.js
@@ -0,0 +1,13 @@
+// pages/AlunoMarketo.js
+import React from 'react';
+
+function AlunoMarketo() {
+  return (
+    <div>
+      <h1>Aluno Marketo</h1>
+      <p>Compre e venda itens entre os alunos.</p>
+    </div>
+  );
+}
+
+export default AlunoMarketo;
---
+++ src/Chat.js
@@ -0,0 +1,13 @@
+// pages/Chat.js
+import React from 'react';
+
+function Chat() {
+  return (
+    <div>
+      <h1>Chat</h1>
+      <p>Converse com seus colegas e professores aqui.</p>
+    </div>
+  );
+}
+
+export default Chat;
