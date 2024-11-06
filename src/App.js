c5e537c1b7fdb3fbbec01977073601d33375af39
Jez <jezekael.brunon@gmail.com>
2024-11-06 11:46:25 +0000

    Perfil

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import PaginaInicial from './pages/PaginaInicial';
@@ -7,19 +6,27 @@ import Aulas from './pages/Aulas';
 import Chat from './pages/Chat';
 import AlunoMarketo from './pages/AlunoMarketo';
 import Noticias from './pages/Noticias';
+import Perfil from './pages/Perfil';
 import './App.css';
+import HomeLogo from './assets/Home.png'; // Import your logo file here
+import ProfileLogo from './assets/Profile.png'; // Import your logo file here
 
 function App() {
   return (
     <Router>
       <div>
         <nav>
-          <Link to="/">Página Inicial</Link>
+          <Link to="/">
+            <img src={HomeLogo} alt="Logo" className="nav-logo" /> {/* Logo for Página Inicial */}
+          </Link>
           <Link to="/calendario">Calendário</Link>
           <Link to="/aulas">Aulas</Link>
           <Link to="/chat">Chat</Link>
           <Link to="/alunomarketo">Aluno Marketo</Link>
           <Link to="/noticias">Notícias</Link>
+          <Link to="/perfil">
+            <img src={ProfileLogo} alt="Logo" className="nav-logo" /> {/* Logo for Perfil */}
+          </Link>
         </nav>
         <div className="container">
           <Routes>
@@ -29,6 +36,7 @@ function App() {
             <Route path="/chat" element={<Chat />} />
             <Route path="/alunomarketo" element={<AlunoMarketo />} />
             <Route path="/noticias" element={<Noticias />} />
+            <Route path="/perfil" element={<Perfil />} />
           </Routes>
         </div>
       </div>
---
+++ src/pages/Perfil.js
@@ -0,0 +1,14 @@
+// pages/Perfil.js
+import React from 'react';
+
+function Perfil() {
+  return (
+    <div>
+      <h1>Nome</h1>
+      <img src="user.jpg" alt="User" width="100" height="100"></img>
+      <p>Aqui estão as informações o user.</p>
+    </div>
+  );
+}
+
+export default Perfil;
